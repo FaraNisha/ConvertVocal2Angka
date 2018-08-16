7f7c00c12b135c5a5bb3b6217a8dcedf63782e6a
WINDOWS 10 <WINDOWS 10@DESKTOP-VQO2UIQ>
2018-08-16 17:35:36 +0700

    initial Commit

---
+++ src/convertvocal2angka/ConvertVocal2Angka.java
@@ -0,0 +1,18 @@
+
+package convertvocal2angka;
+
+import java.util.Scanner;
+public class ConvertVocal2Angka {
+    private static void tampilJudul(String identitas) {
+        System.out.println("Identitas : "+ identitas);
+        
+        System.out.println("\nConvert Kalimat Alay Angka (Vokal Ke Angka)\n");
+
+    }
+    public static void main(String[] args) {
+    String identitas = "Fara Nisha Sukma Gustika / XRPL2 / 14";
+    
+    tampilJudul(identitas);
+    
+}
+}
